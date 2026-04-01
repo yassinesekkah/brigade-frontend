0b5294b0d14b207fb20ae5049537ce79b59abb98
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-04-01 14:01:51 +0100

    feat: create profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import PlatesPage from './pages/PlatesPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import PlateDetails from './pages/PlateDetails';
-
-
-
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import PlatesPage from "./pages/PlatesPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PlateDetails from "./pages/PlateDetails";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from "./pages/Profile";
 
 // ==================== APP ====================
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-gray-50">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/plates" element={<PlatesPage />} />
-            <Route path="/plates/:id" element={<PlateDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen bg-gray-50">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/plates" element={<PlatesPage />} />
+              <Route path="/plates/:id" element={<PlateDetails />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/Profile" element={<ProtectedRoute> <Profile /></ProtectedRoute>}
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (userData, token) => {
+    localStorage.setItem("token", token);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
